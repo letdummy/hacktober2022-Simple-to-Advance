13fd116160497e469646f95e1c984f02b63921ca
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2022-10-02 12:35:59 +0530

    Music Responsive Landing Page

---
+++ Music Site Landing Page/index.js
@@ -0,0 +1,6 @@
+let navigation = document.querySelector(".navigation");
+let burger = document.querySelector(".burger");
+
+burger.addEventListener("click", () => {
+  navigation.classList.toggle("block");
+});
