ce87ef0c67e0ae7b8ce2647afee9ebf6857e1f27
Anurag-Bharati <anuragbharati26@gmail.com>
2022-10-04 01:08:16 +0545

    Added API fetch + animations

---
+++ XX. Dad Joke/script.js
@@ -0,0 +1,30 @@
+const jokeButton = document.querySelector("#jokeBtn");
+const bg = document.querySelector(".bg");
+const rvl = document.querySelector(".card > div:nth-child(1)");
+const loader = document.querySelector(".load");
+
+const joke = document.getElementById("joke");
+
+jokeButton.addEventListener("click", () => {
+    bg.classList.remove("haha");
+    rvl.classList.remove("show");
+    loader.classList.remove("hidden");
+    setTimeout(fetchData, 3000);
+});
+
+function fetchData() {
+    fetch("https://icanhazdadjoke.com", {
+        headers: {
+            Accept: "application/json",
+        },
+    })
+        .then((x) => x.json())
+        .then((data) => {
+            joke.innerHTML = data.joke;
+        })
+        .then((x) => {
+            bg.classList.add("haha");
+            rvl.classList.add("show");
+            loader.classList.add("hidden");
+        });
+}
