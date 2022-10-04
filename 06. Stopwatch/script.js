547d831d5742e407d055257128e31dd1b63b60dc
Razafridi <94115810+Razafridi@users.noreply.github.com>
2022-10-04 12:05:40 +0500

    Update script.js

--- 06. Stopwatch/script.js
+++ 06. Stopwatch/script.js
@@ -1,70 +1,92 @@
 // Making a stopwatch using Javascript
-// Hold the value of the stopwatch
-let seconds = 0;
-let minutes = 0;
-let hours = 0;
+var interval = null;
+var sec = 0
+var min = 0
+var hour = 0
+var isStart = false;
 
-// Hold the interval variable
-let interval = null;
+var clock = document.getElementById("clock")
+var list = document.getElementById('list')
 
 
 
-// Hold the display values
-let displaySeconds = 0;
-let displayMinutes = 0;
-let displayHours = 0;
-
-// Write the stopwatch function(logic to increment the time)
-function stopWatch() {
-
-    seconds++;
-
-    if (seconds / 60 == 1){
-        seconds = 0;
-        minutes++;
-    }
-    if (minutes / 60 == 1){
-        minutes = 0;
-        hours++;
+function start_pause(){
+    if(isStart){
+        isStart = false
+        clearInterval(interval)
+        document.getElementById("start_pause").innerHTML = "Start";
+    }else{
+        isStart = true
+        document.getElementById("start_pause").innerHTML = "Pause";
+        interval = setInterval(count , 1000)
     }
 
-
-
-
-// Add leading 0 to the display value if less than 10
-if (seconds < 10){
-    displaySeconds = "0" + seconds.toString();
-}
-else {
-    displaySeconds = seconds;
-}
-if (minutes < 10){
-    displayMinutes = "0" + minutes.toString();
-}
-else {
-    displayMinutes = minutes;
-}
-if (hours < 10){
-    displayHours = "0" + hours.toString();
-}
-else {
-    displayHours = hours;
 }
 
-document.getElementById("timerdisplay").innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds;
-
+function reset(){
+    sec = 0
+    min =0
+    hour = 0
+    clearInterval(interval)
+    //interval = null
+    displayClock()
+    list.innerHTML = ""
 }
 
-function start() {
-    interval = window.setInterval(stopWatch, 1000);
+function lap(){
+    var format = ""
+    if(hour.toString().length < 2){
+        format += "0"+hour.toString()
+    }else{
+        format +=hour.toString()
+    }
+
+    if(min.toString().length < 2){
+        format += ":0"+min.toString()
+    }else{
+        format +=":"+min.toString()
+    }
+
+    if(sec.toString().length < 2){
+        format += ":0"+sec.toString()+""
+    }else{
+        format +=":"+sec.toString()
+    }
+    list.innerHTML += "<li>"+format+"</li>";
 }
-function stop() {
-    interval = window.clearInterval(interval);
+
+function count(){
+    if(sec >= 60 ){
+        sec= 0;
+        min++;
+    }
+    if(min >= 60){
+        min = 0
+        hour++;
+    }
+    displayClock()
+    sec++;
 }
-function reset() {
-    window.clearInterval(interval);
-    seconds = 0;
-    minutes = 0;
-    hours = 0;
-    document.getElementById("timerdisplay").innerHTML = "00:00:00";
+
+function displayClock(){
+    var format = ""
+
+    if(hour.toString().length < 2){
+        format += "0"+hour.toString()
+    }else{
+        format +=hour.toString()
+    }
+
+    if(min.toString().length < 2){
+        format += ":0"+min.toString()
+    }else{
+        format +=":"+min.toString()
+    }
+
+    if(sec.toString().length < 2){
+        format += ":0"+sec.toString()+""
+    }else{
+        format +=":"+sec.toString()
+    }
+    clock.innerHTML = format
 }
