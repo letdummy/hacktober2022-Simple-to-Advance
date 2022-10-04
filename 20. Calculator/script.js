1deb7e71038a6aedc6816f622fd393df68d96090
Krishna <krishnamadhwani2@gmail.com>
2022-10-04 15:13:31 +0530

    updated calculator

--- 20. Calculator/script.js
+++ 20. Calculator/script.js
@@ -1,75 +1,150 @@
-const nums = document.querySelectorAll(".num");
-const typed = document.querySelector(".type");
-const result = document.querySelector(".result");
-
-let val = "0";
-
-nums.forEach((num) => {
-  num.addEventListener("click", (e) => {
-    typed.classList.remove("active");
-    let char = e.target.innerText;
-    if (val === "0" && !char.match(/[*+/.-]/gi)) {
-      val = char;
-    } else {
-      val += char;
-    }
-
-    if (val.match(/[0-9]+[+-\/*][+-\/*]+/gi)) {
-      const operator = val[val.length - 1];
-      val = val.substring(0, val.length - 2) + operator;
-    }
-
-    typed.innerText = val.replace(/\*/g, "×").replace(/\//g, "÷");
-    if (!val.match(/[0-9]+[+-\/*]$/gi)) {
-      try {
-        result.innerText = eval(val);
-      } catch (error) {
-        invalidExpression();
-      }
-    }
-  });
-});
-
-document.querySelector(".itemAC").addEventListener("click", () => {
-  typed.classList.remove("active");
-  typed.innerText = "0";
-  result.innerText = "";
-  val = "0";
-});
-
-document.querySelector(".item-del").addEventListener("click", () => {
-  typed.classList.remove("active");
-  val = val.substring(0, val.length - 1);
-  if (val) {
-    typed.innerText = val;
-    if (!val.match(/[0-9]+[+-\/*]$/gi)) {
-      try {
-        result.innerText = eval(val);
-      } catch (error) {
-        invalidExpression();
-      }
-    } else {
-      result.innerText = "";
-    }
-  } else {
-    typed.innerText = "0";
-    result.innerText = "";
+let string = "";
+let a=0;
+function toggle()
+{
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+  let cal = document.getElementById("calc");
+  let button = document.getElementsByClassName("button");
+  if(cal.value == "light"){
+     cal.style.backgroundColor = "#99d5ff";
+     cal.value = "black";
+     cal.style.borderColor = "#13695d";
+     
   }
-});
-
-document.querySelector(".item-eq").addEventListener("click", () => {
-  try {
-    val = eval(val).toString();
-    typed.classList.add("active");
-    typed.innerText = val;
-    result.innerText = "";
-  } catch (error) {
-    invalidExpression();
-  }
-});
-
-function invalidExpression() {
-  typed.innerText = "0";
-  result.innerText = "Invalid Expression";
-  val = "0";
+  else{
+    cal.style.backgroundColor = "black";
+    cal.style.borderColor = "#b5d307";
+    cal.value = "light";
+  }    
 }
+
+
+
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  function createitem(name)
+{
+  let li = document.createElement('li');
+  li.textContent = name;
+  return li;
+}
+const history = document.querySelector('#history');
+    button.addEventListener('click', (e)=>{
+      if(e.target.innerHTML == '='){
+        history.appendChild(createitem(string));
+        try {
+            string = eval(string);
+            document.querySelector('input').value = string;
+            
+          }
+          catch(err) {
+            document.querySelector('input').value ='Recheck' ;
+            
+          }
+      }
+      else if(e.target.innerHTML == 'C'){
+        string = ""
+        document.querySelector('input').value = string;
+      }
+      else if(e.target.innerHTML == 'sin')
+      {
+        if(a==1)
+        string=eval(string*Math.PI/180);
+        history.appendChild(createitem('sin('+string+')'));
+        
+        string= Math.sin(string);
+        document.querySelector('input').value = string;
+
+      }
+      else if(e.target.innerHTML == 'cos')
+      {
+        if(a==1)
+        string=eval(string*Math.PI/180);
+        history.appendChild(createitem('cos('+string+')'));
+
+          string= string= Math.cos(string);
+          
+          document.querySelector('input').value = string;
+      }
+      else if(e.target.innerHTML == 'tan')
+      {
+          if(a==1)
+          string=eval(string*Math.PI/180);
+          history.appendChild(createitem('tan('+string+')'));
+          string = Math.tan(string);
+          document.querySelector('input').value = string;
+      }
+      else if(e.target.innerHTML == 'log')
+      {
+          string = Math.log10(string);
+          document.querySelector('input').value = string;
+      }
+      else if(e.target.innerHTML == 'ln')
+      {
+          string = Math.log(string);
+          document.querySelector('input').value = string;
+      }
+      else if(e.target.innerHTML == 'DEL')
+      {
+          string= string.slice(0, string.length - 1);
+          document.querySelector('input').value = string;
+      }
+      else if(e.target.innerHTML == 'RAD')
+      {
+
+        document.getElementById("angle").innerHTML = "DEG";
+        a=1;
+      
+      }
+      else if(e.target.innerHTML == 'DEG')
+      {
+        a=0;
+        document.getElementById("angle").innerHTML = "RAD";
+        
+
+      }
+      else if(e.target.innerHTML == 'INV')
+      {
+        document.getElementById("inv").innerHTML = "NOR";
+        document.getElementById("sin").innerHTML = "asin"; 
+        document.getElementById("cos").innerHTML = "acos";
+        document.getElementById("tan").innerHTML = "atan";
+      }
+      else if(e.target.innerHTML == 'NOR')
+      {
+        document.getElementById("inv").innerHTML = "INV";
+        document.getElementById("sin").innerHTML = "sin"; 
+        document.getElementById("cos").innerHTML = "cos";
+        document.getElementById("tan").innerHTML = "tan";
+      }      
+      else if(e.target.innerHTML == 'asin')
+      {
+        history.appendChild(createitem('asin('+string+')'));
+        string = Math.asin(string);
+        
+        document.querySelector('input').value = string;
+
+      }
+      else if(e.target.innerHTML == 'acos')
+      {
+        history.appendChild(createitem('acos('+string+')'));
+        string = Math.acos(string);
+        document.querySelector('input').value = string;
+
+      }
+      else if(e.target.innerHTML == 'atan')
+      {
+        history.appendChild(createitem('atan('+string+')'));
+        string = Math.atan(string);
+        document.querySelector('input').value = string;
+
+      }
+      else{ 
+      console.log(e.target)
+      string = string + e.target.innerHTML;
+      document.querySelector('input').value = string;
+        }
+
+    })
+  })
